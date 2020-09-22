155218842abf1ea23216675af38f7dcd427905a0
Keven Carlson <kevenrc@gmail.com>
2020-09-22 14:02:11 -0600

    account for variable names nn

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('raw/heloc_dataset_v1.csv')
+df = pd.read_csv('data/raw/heloc_dataset_v1.csv')
 
 variable_names = list(df.columns[1:])
+print(df.describe())
 
 X = df[variable_names].values
 
@@ -16,7 +17,7 @@ y = y.astype(np.int)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-np.savetxt('X_train.csv', X_train, delimiter=',')
-np.savetxt('X_test.csv', X_test, delimiter=',')
-np.savetxt('y_train.csv', y_train, delimiter=',')
-np.savetxt('y_test.csv', y_test, delimiter=',')
+#np.savetxt('X_train.csv', X_train, delimiter=',')
+#np.savetxt('X_test.csv', X_test, delimiter=',')
+#np.savetxt('y_train.csv', y_train, delimiter=',')
+#np.savetxt('y_test.csv', y_test, delimiter=',')
--- scripts/nn_train.py
+++ scripts/nn_train.py
@@ -1,6 +1,9 @@
 from fastai import *
 import numpy as np
 
+raw_data = pd.read_csv('data/raw/heloc_dataset_v1.csv', delimiter=',')
+variable_names = list(raw_data.columns[1:])
+
 X_train = np.genfromtxt('data/preprocessed/X_train.csv', delimiter=',')
 X_test = np.genfromtxt('data/preprocessed/X_test.csv', delimiter=',')
 y_train = np.genfromtxt('data/preprocessed/y_train.csv', delimiter=',')
