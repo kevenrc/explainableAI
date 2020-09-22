cb4d9cddcdc8dd0a5bc955ddef92140ad88495b2
Keven Carlson <kevenrc@gmail.com>
2020-09-22 07:15:32 -0600

    preprocessed data

---
+++ data/data_preprocess.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('raw/heloc_dataset_v1.csv')
+
+variable_names = list(df.columns[1:])
+
+X = df[variable_names].values
+
+y = df.RiskPerformance.values
+mask = y == "Bad"
+y[mask] = 1
+y[~mask] = 0
+y = y.astype(np.int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+np.savetxt('X_train.csv', X_train, delimiter=',')
+np.savetxt('X_test.csv', X_test, delimiter=',')
+np.savetxt('y_train.csv', y_train, delimiter=',')
+np.savetxt('y_test.csv', y_test, delimiter=',')
